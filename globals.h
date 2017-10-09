6a833beb6e8fde6205866f76a533d1d997949aae
Max Forasteiro <max.forasteiro@gmail.com>
2017-10-08 23:10:09 -0300

    Adjusting global file to c minus lang, but i\'m still in doubt about MAXCHILDREN value

--- globals.h
+++ globals.h
@@ -1,5 +1,6 @@
 /****************************************************/
 /* File: globals.h                                  */
+/* Yacc/Bison Version                               */
 /* Global types and vars for C- compiler            */
 /* must come before other include files             */
 /* Compiler Construction: Principles and Practice   */
@@ -14,6 +15,27 @@
 #include <ctype.h>
 #include <string.h>
 
+/* Yacc/Bison generates internally its own values
+ * for the tokens. Other files can access these values
+ * by including the tab.h file generated using the
+ * Yacc/Bison option -d ("generate header")
+ *
+ * The YYPARSER flag prevents inclusion of the tab.h
+ * into the Yacc/Bison output itself
+ */
+
+#ifndef YYPARSER
+
+/* the name of the following file may change */
+#include "cminus.tab.h"
+
+/* ENDFILE is implicitly defined by Yacc/Bison,
+ * and not included in the tab.h file
+ */
+#define ENDFILE 0
+
+#endif
+
 #ifndef FALSE
 #define FALSE 0
 #endif
@@ -23,21 +45,13 @@
 #endif
 
 /* MAXRESERVED = the number of reserved words */
-#define MAXRESERVED 8
+/* See helper file to explain the change between tiny and c- */
+#define MAXRESERVED 6
 
-typedef enum
-    /* book-keeping tokens */
-   {ENDFILE,ERROR,
-    /* reserved words */
-    IF,ELSE,VOID,INT,RETURN,WHILE,
-    /* multicharacter tokens */
-    ID,NUM,
-    /* special symbols */
-    ASSIGN,EQ,NEQ,LT,LET,GT,GET,
-    PLUS,MINUS,TIMES,OVER,
-    LPAREN,RPAREN,LBRACKET,RBRACKET,
-    LBRACE,RBRACE,SEMI,COMMA
-   } TokenType;
+/* Yacc/Bison generates its own integer values
+ * for tokens
+ */
+typedef int TokenType;
 
 extern FILE* source; /* source code text file */
 extern FILE* listing; /* listing output text file */
